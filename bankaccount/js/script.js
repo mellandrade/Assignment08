99e12f1d6b9d216ec7eb9d78ce411896bd064066
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-18 18:53:07 -0700

    Add files via upload
    Update with assignment files

---
+++ bankaccount/js/script.js
@@ -0,0 +1,8 @@
+/*eslint-env browser*/
+
+var name = window.prompt("What is your name?");
+window.document.write(name);
+
+var deposit = window.prompt("Enter the amount you'd like to deposit.");
+
+var withdraw = window.prompt("Enter the amount you'd like to withdraw.");

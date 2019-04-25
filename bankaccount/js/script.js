087f3d29337fdd352976790318721132fa671f2f
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-25 12:41:04 -0700

    Add files via upload
    Update bankaccount

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,8 +1,35 @@
 /*eslint-env browser*/
 
-var name = window.prompt("What is your name?");
-window.document.write(name);
-
-var deposit = window.prompt("Enter the amount you'd like to deposit.");
-
-var withdraw = window.prompt("Enter the amount you'd like to withdraw.");
+function myName() {
+    "use strict";
+    var name = window.prompt("What is your name?");
+    window.document.getElementById("myname").innerHTML = (name);
+}
+var initialBalance = 100;
+function getBalance() {
+    "use strict";
+    window.document.getElementById("balance").innerHTML = (initialBalance);
+}
+function myDeposit() {
+    "use strict";
+    var deposit = parseFloat(window.prompt("Enter the amount you'd like to deposit."));
+    if (isNaN(deposit) || deposit === "") {
+        window.alert("Error. Please enter a number.");
+        myDeposit();
+    } else {
+        initialBalance += deposit;
+        getBalance();
+    }
+}
+function myWithdraw() {
+    "use strict";
+    var withdraw = parseFloat(window.prompt("Enter the amount you'd like to withdraw."));
+    if (isNaN(withdraw) || withdraw === "") {
+        window.alert("Error. Please enter a number.");
+        myWithdraw();
+    } else {
+        initialBalance -= withdraw;
+        getBalance();
+    }
+    
+}
